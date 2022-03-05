4b6dd62748b7438d2e78b9b488acb914b96a6cbf
Courdevelops <courdevelops@gmail.com>
2022-03-05 16:26:58 -0500

    banner

---
+++ src/components/Banner/banner.js
@@ -0,0 +1,13 @@
+import {React} from 'react';
+import './banner.css';
+
+function Banner(){
+    return(
+        <div className='banner-container'>
+            <h4>Saturday, March 5th</h4>
+            <h1>Welcome, CourDevelops</h1>
+        </div>
+    )
+};
+
+export default Banner;
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../App.css';
 
+//Banner//
+import Banner from '../components/Banner/banner';
+
 //Jobs List//
 import Joblist from '../components/Jobs/jobslist';
 
 function Main(){
   return(
     <div className='mainbody'>
+      <Banner/>
       <Joblist/>
     </div>
   )
